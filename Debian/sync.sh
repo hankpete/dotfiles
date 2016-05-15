c01a0b45858d41e485cb21d0dd2a5be449256a0e
Henry Peterson <hpeter@visalia-desktop>
2016-05-15 11:04:09 -0700

    xflux and cp

--- Debian/sync.sh
+++ Debian/sync.sh
@@ -7,24 +7,28 @@ mkdir /home/hpeter/.vim/colors
 mkdir /etc/vim/colors
 mkdir /home/hpeter/.config/i3
 
-echo "creating symbolic links..."
+echo "copying files into directories..."
 
-ln -f vimrc /home/hpeter/.vim/vimrc
+cp vimrc /home/hpeter/.vim/vimrc
 
-ln -f vimrc /etc/vim/vimrc
+cp vimrc /etc/vim/vimrc
 
-ln -f anotherdark.vim /home/hpeter/.vim/colors/anotherdark.vim
+cp anotherdark.vim /home/hpeter/.vim/colors/anotherdark.vim
 
-ln -f anotherdark.vim /etc/vim/colors/anotherdark.vim
+cp anotherdark.vim /etc/vim/colors/anotherdark.vim
 
-ln -f .bashrc /home/hpeter/.bashrc
+cp .bashrc /home/hpeter/.bashrc
 
-ln -f .bashrc.root /root/.bashrc
+cp .bashrc.root /root/.bashrc
 
-ln -f i3status.conf /home/hpeter/.config/i3/i3status.conf
+cp i3status.conf /home/hpeter/.config/i3/i3status.conf
 
-ln -f config /home/hpeter/.config/i3/config
+cp config /home/hpeter/.config/i3/config
 
-ln -f .Xresources /home/hpeter/.Xresources
+cp .Xresources /home/hpeter/.Xresources
+
+cp housekeeping /usr/bin/housekeeping
+
+cp xflux /usr/bin/xflux
 
 echo "done."
